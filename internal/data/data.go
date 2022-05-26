98a9410273b0d488fa81fedd3a96d0c7e99efdf4
jojo-jie <18624275868@163.com>
2022-05-26 19:55:43 +0800

    jaeger bugfix

--- internal/data/data.go
+++ internal/data/data.go
@@ -3,16 +3,18 @@ package data
 import (
 	"ariga.io/sqlcomment"
 	"context"
-	"entgo.io/ent/dialect"
 	"entgo.io/ent/dialect/sql"
+	"fmt"
 	"github.com/go-kratos/kratos/v2/log"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/wire"
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
-	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"spider/ent"
 	"spider/internal/conf"
+	"time"
 )
 
 // ProviderSet is data providers.
@@ -24,58 +26,51 @@ type Data struct {
 	client *ent.Client
 }
 
+type CustomCommenter struct{}
+
+func (mcc CustomCommenter) Tag(ctx context.Context) sqlcomment.Tags {
+	return sqlcomment.Tags{
+		"key": "value",
+	}
+}
+
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	defer func() {
-		if tp, ok := otel.GetTracerProvider().(*tracesdk.TracerProvider); ok {
-			tp.Shutdown(context.Background())
-		}
-	}()
 	drv, err := sql.Open(c.Database.GetDriver(), c.Database.GetSource())
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "sql open fail")
 	}
 	// 获取数据库驱动中的sql.DB对象
-	/*db := drv.DB()
+	db := drv.DB()
 	db.SetMaxIdleConns(int(c.GetDatabase().GetMaxIdleConns()))
 	db.SetMaxOpenConns(int(c.GetDatabase().GetMaxOpenConns()))
 	db.SetConnMaxLifetime(time.Duration(c.GetDatabase().GetConnMaxLifetime().Seconds))
-	*/
 
-	commentedDriver := sqlcomment.NewDriver(dialect.Debug(drv),
-		sqlcomment.WithTagger(
-			// add tracing info with Open Telemetry.
-			sqlcomment.NewOTELTagger(),
-			// use your custom commenter
-
-		),
-		// add `db_driver` version tag
-		sqlcomment.WithDriverVerTag(),
-		// add some global tags to all queries
-		sqlcomment.WithTags(sqlcomment.Tags{
-			sqlcomment.KeyApplication: "bootcamp",
-			sqlcomment.KeyFramework:   "go-chi",
-		}))
-
-	client := ent.NewClient(ent.Driver(commentedDriver))
-
+	client := ent.NewClient(ent.Driver(drv))
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 		client.Close()
-		//db.Close()
+		db.Close()
 	}
 	if err := client.Schema.Create(context.Background()); err != nil {
 		return nil, nil, errors.Wrap(err, "client Schema fail")
 	}
+
 	// Add a global hook that runs on all types and all operations.
-	/*client.Debug().Use(func(next ent.Mutator) ent.Mutator {
+	client.Debug().Use(func(next ent.Mutator) ent.Mutator {
 		return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
 			start := time.Now()
 			defer func() {
-				log.NewHelper(logger).Infof("Op=%s\tType=%s\tTime=%s\tConcreteType=%T\n", m.Op(), m.Type(), time.Since(start), m)
+				msg := fmt.Sprintf("Op=%s\tType=%s\tTime=%s\tConcreteType=%T\n", m.Op(), m.Type(), time.Since(start), m)
+				log.NewHelper(logger).Infof(msg)
+				_, span := otel.GetTracerProvider().Tracer("").Start(ctx, "ent sql")
+				span.SetAttributes(attribute.String("sql_msg", msg))
+				span.SetName("zzzyf")
+				span.SetStatus(codes.Unset, "ok")
+				defer span.End()
 			}()
 			return next.Mutate(ctx, m)
 		})
-	})*/
+	})
 	return &Data{client: client}, cleanup, nil
 }
